3fe5f88093506f921754ca466aee55d483d23aaa
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-04-14 17:04:32 -0300

    remove console log

--- src/middlewares/error-handling-middleware.ts
+++ src/middlewares/error-handling-middleware.ts
@@ -3,7 +3,6 @@ import httpStatus from 'http-status';
 import { ApplicationError } from '@/protocols';
 
 export function handleApplicationErrors(err: ApplicationError | Error, _req: Request, res: Response) {
-  console.log('entrei no handle application error');
   if (err.name === 'CannotEnrollBeforeStartDateError') {
     return res.status(httpStatus.BAD_REQUEST).send({
       message: err.message,
