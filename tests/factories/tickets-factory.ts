d459817b2935d5220bf7a7a8c7bf795e878f09a7
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-04-24 18:37:36 -0300

    fix: createTicket factory change || for ??

--- tests/factories/tickets-factory.ts
+++ tests/factories/tickets-factory.ts
@@ -7,8 +7,8 @@ export async function createTicketType(isRemote?: boolean, includesHotel?: boole
     data: {
       name: faker.name.findName(),
       price: faker.datatype.number(),
-      isRemote: isRemote || faker.datatype.boolean(),
-      includesHotel: includesHotel || faker.datatype.boolean(),
+      isRemote: isRemote ?? faker.datatype.boolean(),
+      includesHotel: includesHotel ?? faker.datatype.boolean(),
     },
   });
 }
