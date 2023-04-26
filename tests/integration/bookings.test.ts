ac858a615a5241e5bce7c5e64d22a41b74c69731
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-04-26 11:10:03 -0300

    feat: test GET /booking; missing data & success cs

--- tests/integration/bookings.test.ts
+++ tests/integration/bookings.test.ts
@@ -2,8 +2,17 @@ import supertest from 'supertest';
 import httpStatus from 'http-status';
 import faker from '@faker-js/faker/locale/pt_BR';
 import * as jwt from 'jsonwebtoken';
-import { cleanDb } from '../helpers';
-import { createUser } from '../factories';
+import { TicketStatus } from '@prisma/client';
+import { cleanDb, generateValidToken } from '../helpers';
+import {
+  createBooking,
+  createEnrollmentWithAddress,
+  createHotel,
+  createRooms,
+  createTicket,
+  createTicketType,
+  createUser,
+} from '../factories';
 import app, { init } from '@/app';
 
 const server = supertest(app);
@@ -31,6 +40,40 @@ describe('GET /booking', () => {
       const response = await server.get('/booking').set('Authorization', `Bearer ${token}`);
 
       expect(response.status).toEqual(httpStatus.UNAUTHORIZED);
+    });
+  });
+
+  describe('data is missing', () => {
+    it('Should respond 404 if no booking', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const response = await server.get('/booking').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toEqual(httpStatus.NOT_FOUND);
+    });
+  });
+  describe('success case', () => {
+    it('Should respond 200 and booking info', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketType(false, true);
+      await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      const hotel = await createHotel();
+      const rooms = await createRooms(hotel.id, 3);
+      const booking = await createBooking(user.id, rooms[0].id);
+      const expectedOutput = {
+        id: booking.id,
+        Room: {
+          ...booking.Room,
+          updatedAt: booking.Room.updatedAt.toISOString(),
+          createdAt: booking.Room.createdAt.toISOString(),
+        },
+      };
+      const response = await server.get('/booking').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toEqual(httpStatus.OK);
+      expect(response.body).toEqual(expectedOutput);
     });
   });
 });
