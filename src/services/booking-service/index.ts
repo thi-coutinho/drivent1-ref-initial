a54c1739692d40d11e86597a0b40f11eefcda573
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-04-26 17:42:16 -0300

    feat: bookingService create booking & validation

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -1,5 +1,8 @@
-import { notFoundError } from '@/errors';
+import { TicketStatus } from '@prisma/client';
+import { invalidBookingError, notFoundError, roomFullyBookedError } from '@/errors';
 import bookingRepository from '@/repositories/booking-repository';
+import hotelRepository from '@/repositories/hotel-repository';
+import ticketRepository from '@/repositories/ticket-repository';
 
 async function getByUserId(userId: number) {
   const booking = await bookingRepository.find(userId);
@@ -8,8 +11,30 @@ async function getByUserId(userId: number) {
   return outputBooking;
 }
 
+async function create(userId: number, roomId: number) {
+  await validateBooking(userId, roomId);
+  const bookingId = await bookingRepository.create(userId, roomId);
+  return bookingId;
+}
+
+async function validateBooking(userId: number, roomId: number) {
+  const room = await hotelRepository.findRoom(roomId);
+  if (!room) throw notFoundError();
+  if (room.capacity <= room.Booking.length) throw roomFullyBookedError();
+  const ticket = await ticketRepository.findFirstTicketByUser(userId);
+  if (
+    !ticket ||
+    ticket.status !== TicketStatus.PAID ||
+    ticket.TicketType.isRemote ||
+    !ticket.TicketType.includesHotel
+  ) {
+    throw invalidBookingError();
+  }
+}
+
 const bookingService = {
   getByUserId,
+  create,
 };
 
 export default bookingService;
