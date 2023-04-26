c387ac7c4b13316b73d3dc9e0c1252d834545de3
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-04-26 11:03:48 -0300

    fix: GET /booking response data

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -1,14 +1,12 @@
 import { NextFunction, Response } from 'express';
-import { Booking, Room } from '@prisma/client';
+import { Room } from '@prisma/client';
 import { AuthenticatedRequest } from '@/middlewares';
 import bookingService from '@/services/booking-service';
 
 export async function getByUserId(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   const { userId } = req;
   try {
-    const booking: Booking & {
-      Room: Room;
-    } = await bookingService.getByUserId(userId);
+    const booking: { id: number; Room: Room } = await bookingService.getByUserId(userId);
     res.send(booking);
   } catch (error) {
     next(error);
--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -4,7 +4,8 @@ import bookingRepository from '@/repositories/booking-repository';
 async function getByUserId(userId: number) {
   const booking = await bookingRepository.find(userId);
   if (!booking) throw notFoundError();
-  return booking;
+  const outputBooking = { id: booking.id, Room: booking.Room };
+  return outputBooking;
 }
 
 const bookingService = {
